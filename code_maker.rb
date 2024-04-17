d34a1b70882520011dd1f04181fdc52fbb7d9a0a
Belbut <80793911+Belbut@users.noreply.github.com>
2024-04-17 18:50:56 +0100

    Complete full game logic

--- code_maker.rb
+++ code_maker.rb
@@ -1,34 +1,25 @@
+require_relative 'player'
+
 class ComputerMaker
   def initialize; end
 
   def create_code
-    Array.new(CodeMaker::SIZE_OF_CODE) { rand(CodeMaker::RANGE_OF_USABLE_NUMBERS) }
+    Array.new(Game::SIZE_OF_CODE) { rand(Game::RANGE_OF_USABLE_NUMBERS) }
   end
 end
 
-class PlayerMaker
-  def initialize; end
+class PlayerMaker < Player
+  def initialize
+    super
+  end
 
   def create_code
     puts 'Hello Mastermind'
-    loop do
-      puts "Pick #{CodeMaker::SIZE_OF_CODE} numbers between #{CodeMaker::RANGE_OF_USABLE_NUMBERS} to make your code:"
-      code = gets.chomp.split('')
-      code.map!(&:to_i)
-
-      return code if code.size == CodeMaker::SIZE_OF_CODE && code.all? do |n|
-                       CodeMaker::RANGE_OF_USABLE_NUMBERS.include?(n)
-                     end
-
-      puts "That wasn't a proper code"
-    end
+    pick_code("Pick #{Game::SIZE_OF_CODE} numbers between #{Game::RANGE_OF_USABLE_NUMBERS} to make your code:")
   end
 end
 
 class CodeMaker
-  SIZE_OF_CODE = 4
-  RANGE_OF_USABLE_NUMBERS = (1..6).freeze
-
   def initialize(is_computer)
     @maker = if is_computer
                ComputerMaker.new
--- game.rb
+++ game.rb
@@ -2,11 +2,95 @@ require_relative 'code_maker'
 require_relative 'code_breaker'
 
 class Game
-  def initialize(maker_computer: true, breaker_computer: true)
+  SIZE_OF_CODE = 4
+  RANGE_OF_USABLE_NUMBERS = (1..6).freeze
+
+  attr_accessor :round
+
+  def initialize(maker_computer, breaker_computer)
     @maker = CodeMaker.new(maker_computer)
     @breaker = CodeBreaker.new(breaker_computer)
     @code = @maker.create_code
+    @round = 1
+  end
+
+  def feedback(guess)
+    code_copy = @code.dup # Make a copy of the @code array to avoid modifying the original
+    guess_copy = guess.dup
+    result = { number_of_correct: 0, number_of_partial: 0 }
+
+    guess.each.with_index do |guess_element, index|
+      if guess_element == @code[index]
+        code_copy.delete_at(code_copy.index(guess_element) || code_copy.length)
+        guess_copy.delete_at(index)
+        result[:number_of_correct] += 1
+      end
+    end
+
+    guess_copy.each.with_index do |guess_element, index|
+      if code_copy.include? guess_element
+        code_copy.delete_at(code_copy.index(guess_element))
+        result[:number_of_partial] += 1
+      end
+    end
+    # puts "You got #{number_of_correct} red heads and #{number_of_partial} white heads from the guess"
+    # puts "#{'■ ' * number_of_correct} #{'□ ' * number_of_partial}"
+
+    result
+  end
+
+  def play_round
+    puts '_______________________________'
+    puts "This is round number #{@round}"
+    guess = @breaker.make_guess
+    puts "The Guess is : #{guess}"
+    p feedback(guess)
+    guess
+  end
+
+  def winner?(guess)
+    p @code
+    p guess
+    @code == guess
+  end
+
+  def end_game(won)
+    if won
+      puts "The code Breaker won the game in #{@round}, congratulations!"
+    else
+      puts " You didn't discover the code. The code was #{@code}"
+    end
+  end
+
+  def self.play
+    maker_computer = Game.prompt_for_player('Code Maker')
+    breaker_computer = Game.prompt_for_player('Code Breaker')
+    game = Game.new(maker_computer, breaker_computer)
+
+    12.times do
+      guess = game.play_round
+
+      if game.winner?(guess)
+        game.end_game(true)
+        return
+      end
+      game.round += 1
+    end
+    game.end_game(false)
+  end
+
+  # prompt will return true if the intention is for the computer to assume the position
+  def self.prompt_for_player(job)
+    puts "Do you want the #{job} to be played by the computer? Y/N ?"
+    loop do
+      answer = gets.chomp.capitalize
+
+      return true if answer == 'Y'
+      return false if answer == 'N'
+
+      puts "Sorry i didn't understood"
+    end
   end
 end
 
-p Game.new
+a = Game.play
