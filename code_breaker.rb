11da83044283d71124ae20250ae2cc1088cc8abc
Belbut <80793911+Belbut@users.noreply.github.com>
2024-04-17 18:46:47 +0100

    Add code breaker

--- code_breaker.rb
+++ code_breaker.rb
@@ -1,5 +1,31 @@
+class ComputerBreaker
+  def initialize; end
+
+  def make_guess
+    Array.new(Game::SIZE_OF_CODE) { rand(Game::RANGE_OF_USABLE_NUMBERS) }
+  end
+end
+
+class PlayerBreaker < Player
+  def initialize
+    super
+  end
+
+  def make_guess
+    pick_code('Make a guess:')
+  end
+end
+
 class CodeBreaker
   def initialize(is_computer)
-    
+    @breaker = if is_computer
+                 ComputerBreaker.new
+               else
+                 PlayerBreaker.new
+               end
+  end
+
+  def make_guess
+    @breaker.make_guess
   end
 end
